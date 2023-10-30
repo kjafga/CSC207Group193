602723162b008568d6dd5dca248fee501ddbff55
buttcloth <gabeof01@gmail.com>
2023-10-30 17:44:28 -0400

    Extremely basic view of chessboard

---
+++ src/view/View.java
@@ -0,0 +1,20 @@
+package view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class View extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(View.class.getResource("view.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        //primaryStage.setResizable(false);
+    }
+
+}
