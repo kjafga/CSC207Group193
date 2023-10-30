00465cf35754b1f046063086c237a0a5693b8e4e
Simon Noble <expectedb@outlook.com>
2023-10-30 18:34:34 -0400

    Added classes that make up the legal moves use case. These classes are placeholders that will be implemented later.

---
+++ src/useCase/legalMoves/LegalMovesInputData.java
@@ -0,0 +1,4 @@
+package useCase.legalMoves;
+
+public class LegalMovesInputData {
+}
---
+++ src/useCase/legalMoves/LegalMovesInteractor.java
@@ -0,0 +1,12 @@
+package useCase.legalMoves;
+
+
+/**
+ *
+ * Takes in a coordinate on the board and returns a list of legal moves
+ *
+ */
+
+
+public class LegalMovesInteractor {
+}
