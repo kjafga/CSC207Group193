7b8b403a47d99fc69808f308230556dda01b01bc
Daniel Lee <danielm.lee@mail.utoronto.ca>
2023-10-30 17:43:09 -0400

    Initial commit

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+module com.example {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires annotations;
+
+    opens view to javafx.fxml;
+    exports view;
+}
