6fa3b304708121ce0de19fc781b4d49a4b039af6
Daniel Lee <danielm.lee@mail.utoronto.ca>
2023-10-30 18:12:49 -0400

    Comment out requires org.controlsfx.controls

--- src/module-info.java
+++ src/module-info.java
@@ -2,7 +2,7 @@ module com.example {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.controlsfx.controls;
+//  requires org.controlsfx.controls;
     requires annotations;
 
     opens view to javafx.fxml;
