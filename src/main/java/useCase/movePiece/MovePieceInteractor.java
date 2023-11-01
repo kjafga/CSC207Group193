865fc986a2fbea8d431c70e1c920fd17a4ae58a4
Simon-Noble <simon.noble@mail.utoronto.ca>
2023-11-01 15:27:52 -0400

    Added to the docstring for the MovePieceInteractor, specifying that the interactor should confirm that the move is legal.

--- src/main/java/useCase/movePiece/MovePieceInteractor.java
+++ src/main/java/useCase/movePiece/MovePieceInteractor.java
@@ -4,7 +4,7 @@ package useCase.movePiece;
 /**
  *
  * Takes in a string representing a move and takes that action on the board
- *
+ * The interactor will check if a move is valid, and send a fail condition if it is not a valid move
  */
 
 public class MovePieceInteractor implements MovePieceInputBoundry{
