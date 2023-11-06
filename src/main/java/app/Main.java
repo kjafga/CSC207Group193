5e660fc5c750edd1d2df6e307ef5e0dd8ff9093e
buttcloth <gabeof01@gmail.com>
2023-11-05 21:44:35 -0500

    added main.java and modified view.java

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,9 @@
+package app;
+
+import view.View;
+
+public class Main {
+    public static void main(String[] args) {
+        View.main(args);
+    }
+}
--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -7,6 +7,10 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class View extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
     
     @Override
     public void start(Stage primaryStage) throws Exception {
