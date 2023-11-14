358722f3111b645270bf69becb7f5490f9f2854f
Daniel Lee <danielm.lee@mail.utoronto.ca>
2023-11-14 11:28:18 -0500

    Add perft tests + modify FEN generation test

--- src/main/test/entity/FENGeneratorTest.java
+++ src/main/test/entity/FENGeneratorTest.java
@@ -8,9 +8,7 @@ public class FENGeneratorTest {
 
     @Test
     public void rootPosTest() {
-        BoardData startPos = new BoardData();
-        FENGenerator gen = new FENGenerator(startPos);
-        assertEquals("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", gen.toString());
+        assertEquals("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", new Board().toString());
     }
 
 }
