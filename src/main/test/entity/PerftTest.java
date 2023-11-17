10f1a049c3b8d211bd7f7e8fdfad1e237d591279
Daniel Lee <danielm.lee@mail.utoronto.ca>
2023-11-17 03:38:55 -0500

    Add perft test + remove FEN generation test
    The perft tests already test FEN generation.

--- src/main/test/entity/PerftTest.java
+++ src/main/test/entity/PerftTest.java
@@ -182,13 +182,18 @@ public class PerftTest {
     }
 
     @Test
+    public void pos4reversed() throws IOException {
+        comparePerft("r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1", 6);
+    }
+
+    @Test
     public void pos5() throws IOException {
         comparePerft("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8", 5);
     }
 
     @Test
     public void pos6() throws IOException {
-        comparePerft("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 7);
+        comparePerft("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 6);
     }
 
 }
