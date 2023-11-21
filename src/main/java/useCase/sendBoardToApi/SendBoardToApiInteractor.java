38689f8b088b58514498a09f1731b93ac6ebf874
Daniel Lee <danielm.lee@mail.utoronto.ca>
2023-11-20 21:08:59 -0500

    Go back to using regex instead of hacky split

--- src/main/java/useCase/sendBoardToApi/SendBoardToApiInteractor.java
+++ src/main/java/useCase/sendBoardToApi/SendBoardToApiInteractor.java
@@ -7,8 +7,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SendBoardToApiInteractor implements SendBoardToApiInputBoundary {
+
+    private static final String STOCKFISH_API_URL = "https://stockfish.online/api/stockfish.php?fen=%s&depth=%d&mode=bestmove";
+    private static final Pattern BESTMOVE_PATTERN = Pattern.compile("bestmove\\s++([a-h][1-8][a-h][1-8][qrnb]?+)");
 
     private final SendBoardToApiOutputBoundary sendBoardToApiOutputBoundary;
     private final Board board;
@@ -20,20 +25,7 @@ public class SendBoardToApiInteractor implements SendBoardToApiInputBoundary {
 
     @Override
     public void execute() throws IOException {
-        // noinspection deprecation: We want to use URL so that we don't have to manually escape the FEN
-        URL url = new URL(
-                String.format("https://stockfish.online/api/stockfish.php?fen=%s&depth=%d&mode=bestmove", board, 13));
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-
-        String move;
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-            String line = reader.readLine();
-            // TODO XXX HACK HACK HACK
-            System.err.println(line);
-            move = line.split(" ")[2];
-            System.err.println(move);
-        }
+        String move = getBestMove(board);
 
         int startSquare = move.charAt(0) - 'a' + ((move.charAt(1) - '1') << 3);
         int endSquare = move.charAt(2) - 'a' + ((move.charAt(3) - '1') << 3);
@@ -43,4 +35,21 @@ public class SendBoardToApiInteractor implements SendBoardToApiInputBoundary {
         SendBoardToApiOutputData outputData = new SendBoardToApiOutputData(board.toString().split(" ")[0]);
         sendBoardToApiOutputBoundary.prepareSuccessView(outputData);
     }
+
+    private static String getBestMove(Board board) throws IOException {
+        // noinspection deprecation: We want to use URL so that we don't have to manually escape the FEN
+        URL url = new URL(String.format(STOCKFISH_API_URL, board, 13));
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+            String line = reader.readLine();
+            Matcher m = BESTMOVE_PATTERN.matcher(line);
+            if (!m.find()) {
+                throw new IllegalStateException("Stockfish API sent line: " + line);
+            }
+            return m.group(1);
+        }
+    }
+
 }
